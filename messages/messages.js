a35362e877d178312e8dfe06360e3e24c3bd31c3
Denis Sokolov <denis@sokolov.cc>
2010-06-16 00:15:07 +0300

    Fixed small bug with message 1.

--- messages/messages.js
+++ messages/messages.js
@@ -2,8 +2,11 @@
 // Show messages for the user
 if (!('messages-seen' in localStorage))
 	localStorage['messages-seen'] = 0;
-if (localStorage['messages-seen'] < 1)
+
+// 1 is skipped due to a bug in version 2.0.
+	
+if (localStorage['messages-seen'] < 2)
 {
 	chrome.tabs.create({ 'url': 'options/options.html' });
-	localStorage['messages-seen'] = 1;
+	// 	Do not update message count, options page will do that.
 }
