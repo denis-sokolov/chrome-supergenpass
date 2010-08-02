61e80f06de02ad82b8dde5c7932d87eb63e09271
Denis Sokolov <denis@sokolov.cc>
2010-08-02 16:44:08 +0300

    Forgotten copyright notice.

--- page/work.js
+++ page/work.js
@@ -1,3 +1,28 @@
+/*!
+ChromeGenPass = Google Chrome + SuperGenPass love.
+Copyright (C) 2010 Denis Sokolov http://sokolov.cc
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+ * All styles are in here not because I don't like separation, but because
+ * I don't want to pollute DOM with extra classes.
+ * Though that way would be clener and I'd prefer it in any other case,
+ * I'd like to be as stealthy as possible.
+ */
+
 function work(selector){
 	chrome.extension.sendRequest({ 'init': true }, function(response) {
 		// Globalize jQuery
