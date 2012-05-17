5249695c670f77741815f7665e07b0f9b83e8943
Denis Sokolov <denis@sokolov.cc>
2012-05-17 16:30:41 +0200

    Page action: cleanup and style.

--- pageAction/pageAction.js
+++ pageAction/pageAction.js
@@ -21,178 +21,31 @@ chrome.extension.sendRequest({ 'init': true }, function(response) {
 	window.jQuery = jQuery;
 	passwords = response['passwords'];
 
-	$('#options a').click(function(){
-		chrome.tabs.create({ 'url': '../options/options.html' });
+	var $domain = $('#domain');
+	var $pass = $('#pass');
+	var $length = $('#length');
+	var $result = $('#result');
+
+	$('body').on('change', 'input', function(){
+		var pass = $pass.val();
+		var domain = $domain.val();
+		var length = $length.val();
+		if (!pass || !domain) return;
+		if (!length || length < 3) return;
+		$result.val(supergenpass(pass, domain, length));
+	});
+	$pass.add($domain).add($length).on('keyup', function(){
+		$(this).trigger('change');
 	});
 
-	var password = (function(){
-		var me = $('#password .password');
-		var len = $('#length');
-		var currentIndex = null;
-
-		var checkPassword = function(e){
-			var pass = passwords[currentIndex];
-			var entered = me.val();
-			if (hash(entered) == pass['hash'])
-			{
-				var generated = supergenpass(entered, hostname.val(), pass['len']);
-				password.close();
-				result.enter(generated);
-				chrome.extension.sendRequest({
-					'store-password': entered,
-					'index': currentIndex
-				});
-			}
-		}
-		var submitCustom = function(e){
-			e.preventDefault();
-			var entered = me.val();
-			var generated = supergenpass(entered, hostname.val(), len.val());
-			result.enter(generated);
-			password.close();
-		}
-		var checkEnter = function(e){
-			if (e.keyCode == 13)
-				submitCustom(e);
-		}
-
-		len.bind('keyup.password', checkEnter);
-
-		return {
-			'open': function(index){
-				me.unbind('.password');
-				currentIndex = index;
-				me.val('').addClass('open').removeAttr('disabled');
-
-				if (index >= 0)
-				{
-					me.bind('keyup.password', checkPassword);
-					len.removeClass('open').attr('disabled', 'disabled');
-				}
-				else
-				{
-					me.bind('keyup.password', checkEnter);
-					result.el.bind('click.password', submitCustom);
-					len.addClass('open').removeAttr('disabled');
-				}
-
-				setTimeout(function(){
-					me.focus();
-				}, 100)
-			},
-			'close': function(){
-				me.add(len).blur().removeClass('open').attr('disabled', 'disabled');
-			}
-		}
-	})();
-
-	var result = (function(){
-		var container = $('#result');
-		var me = container.find('input');
-		var stored = null;
-		return {
-			'el': me,
-			'enter': function(pass){
-				password.close();
-			 	stored = pass;
-				me.val(pass);
-				container.addClass('filled');
-				// me.focus(); // Does not work as expected.
-			},
-			'reset': function(){
-				me.val('');
-				stored = null;
-				container.removeClass('filled');
-			},
-			'val': function(){
-				return stored;
-			},
-		}
-	})();
-
-	var filled = (function(){
-		var me = $('#result a');
-		me.bind('click.fill', function(e){
-			e.preventDefault();
-			chrome.tabs.getSelected(null, function(tab){
-				chrome.tabs.sendRequest(tab.id, {
-					'fill': result.val()
-				});
-			});
-		});
-		return null;
-	})();
-
-	var hostname = (function(){
-		var a = $('#url a');
-		a.click(function(e){
-			e.preventDefault();
-			me.show();
-			a.remove();
-		});
-
-		var me = $('#url input');
-		me.change(function(){
-			result.reset();
-			buttons.find('li').removeClass('current');
-			password.close();
-		});
-
-		chrome.tabs.getSelected(null, function(tab) {
-			var hostNameFinder = new RegExp('//([^\/]+)/');
-			var hostname = hostNameFinder.exec(tab.url)[1];
-			a.find('span').text(hostname);
-			me.val(hostname)
-		});
-
-		return {
-			'val': function(){
-				return me.val();
-			}
-		}
-	})();
-
-	// Buttons
-	var buttons = $('#buttons');
-	var template = buttons.find('.new');
-	var last = buttons.find('.last');
-	for (i in passwords)
-	{
-		template.clone().removeClass('new')
-			.find('a').text(passwords[i]['note']).end()
-			.data('index', i)
-			.insertBefore(last);
-	}
-	buttons.delegate('li', 'click', function(){
+	$('.predef').on('click', 'button', function(){
 		var me = $(this);
-
-		buttons.find('li').removeClass('current');
-		me.addClass('current');
-		result.reset();
-
-		if (me.hasClass('last'))
-		{
-			password.open(-1);
-		}
-		else
-		{
-			var index = me.data('index');
-			chrome.extension.sendRequest({
-				'password': index,
-				'silent': true,
-				'hostname': hostname.val()
-				}, function(response) {
-					if ('hash' in response)
-						result.enter(response['hash']);
-					else
-						password.open(index);
-			});
-		}
+		me.closest('p').find('input').val(me.val()).change();
 	});
 
-	if (!passwords.length)
-	{
-		$('li.last').click();
-		$('#buttons').remove();
-	}
+	// Prefill domain field
+	chrome.tabs.getSelected(null, function(tab) {
+		var hostNameFinder = new RegExp('//([^\/]+)/');
+		$domain.val(hostNameFinder.exec(tab.url)[1]);
+	});
 });
