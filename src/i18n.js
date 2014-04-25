22ac07d255dbb5740a1c090b9c14d7e729af94c6
Denis Sokolov <denis@sokolov.cc>
2014-04-25 22:43:16 +0900

    i18n: html

--- src/i18n.js
+++ src/i18n.js
@@ -6,6 +6,10 @@
  * i18n(message_code)
  *   Returns a text value for message_code
  *
+ *   i18n.html(element, message_code, htmls)
+ *     Fills the element with a text value for message_code, with %n replaced
+ *     by raw html snippets from htmls, returns the element
+ *
  *   i18n.page(jQuery)
  *     Runs using jQuery on a page and populates all data-*-msg values.
  *     Use data-msg=key, data-value-msg, and data-placeholder-msg.
@@ -17,19 +21,28 @@
 		return chrome.i18n.getMessage(name) || ('[' + name + ']');
 	};
 
+	var html = function(el, key, htmls){
+		htmls = htmls || [];
+
+		var m = el.text(msg(key)).html();
+		htmls.forEach(function(replacement, index){
+			m = m.replace('%'+(index+1), replacement.trim());
+		});
+
+		return el.html(m);
+	};
+
 
 	var api = msg;
+
+	api.html = html;
 
 	api.page = function($){
 		$('[data-msg]').each(function(){
 			var el = $(this);
 			var args = el.data('msg').split(';');
-			var m = msg(args.shift());
-			m = el.text(m).html();
-			args.forEach(function(replacement, index){
-				m = m.replace('%'+(index+1), replacement.trim());
-			});
-			el.html(m);
+			var key = args.shift();
+			html(el, key, args);
 		});
 		$('[data-value-msg]').each(function(){
 			var el = $(this);
