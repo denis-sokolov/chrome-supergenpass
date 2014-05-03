9066e94cd06a7b293e8c2b94dc1fc2909a222fdd
Denis Sokolov <denis@sokolov.cc>
2014-05-03 12:55:53 +0900

    Organization: move 3rd-party code to 3rd/

--- gulpfile.js
+++ gulpfile.js
@@ -12,13 +12,12 @@ gulp.task('clean', function(){
 gulp.task('default', function() {
 	gulp.src([
 		'_locales',
+		'3rd',
 		'img',
 		'src',
-		'jquery.min.js',
 		'LICENSE',
 		'manifest.json',
 		'README.md',
-		'supergenpass.min.js'
 	])
 		.pipe(zip('dist.zip'))
 		.pipe(gulp.dest('.'));
@@ -29,7 +28,7 @@ gulp.task('jquery', shell.task([
 	'cd jquery && npm install',
 	'./jquery/node_modules/.bin/grunt --gruntfile jquery/Gruntfile.js' +
 		' custom:-ajax,-deprecated,-effects,-deferred,-wrap,-event/alias,-core/ready',
-	'grep -v sourceMappingURL jquery/dist/jquery.min.js > jquery.min.js',
+	'grep -v sourceMappingURL jquery/dist/jquery.min.js > 3rd/jquery.min.js',
 	'rm -rf jquery'
 ]));
 
@@ -37,6 +36,6 @@ gulp.task('supergenpass', shell.task([
 	'git clone --depth=1 git@github.com:chriszarate/supergenpass-lib.git',
 	'cd supergenpass-lib && npm install',
 	'cd supergenpass-lib && ./node_modules/.bin/gulp browserify',
-	'mv ./supergenpass-lib/dist/supergenpass.browser.js supergenpass.min.js',
+	'mv ./supergenpass-lib/dist/supergenpass.browser.js 3rd/supergenpass.min.js',
 	'rm -rf supergenpass-lib'
 ]));
--- src/events.js
+++ src/events.js
@@ -15,7 +15,7 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
 			}
 
 			var tabId = sender.tab && sender.tab.id;
-			chrome.tabs.executeScript(tabId, {file: 'jquery.min.js', allFrames: true});
+			chrome.tabs.executeScript(tabId, {file: '3rd/jquery.min.js', allFrames: true});
 			chrome.tabs.executeScript(tabId, {file: 'src/lib/i18n.js', allFrames: true});
 			chrome.tabs.executeScript(tabId, {file: 'src/page/script.js', allFrames: true});
 			chrome.tabs.insertCSS(tabId, {file: 'src/page/styles.css', allFrames: true});
