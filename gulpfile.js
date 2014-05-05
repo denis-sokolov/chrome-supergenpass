45dbcbe0fd66cebe4778b9f0faec5fd33ede63ab
Denis Sokolov <denis@sokolov.cc>
2014-05-05 23:14:00 +0900

    gulp: only use stable supergenpass versions

--- gulpfile.js
+++ gulpfile.js
@@ -36,9 +36,13 @@ gulp.task('jquery', shell.task([
 ]));
 
 gulp.task('supergenpass', shell.task([
-	'git clone --depth=1 git@github.com:chriszarate/supergenpass-lib.git',
+	'git clone --depth=1 --branch' +
+		' v$(git ls-remote --tags git@github.com:chriszarate/supergenpass-lib.git |' +
+			' cut -f 2 | cut -d/ -f 3 | grep -o -E \'(\\d|\\.)+$\' |' +
+			' sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | tail -n 1)' +
+		' git@github.com:chriszarate/supergenpass-lib.git',
 	'cd supergenpass-lib && npm install',
 	'cd supergenpass-lib && ./node_modules/.bin/gulp browserify',
-	'mv ./supergenpass-lib/dist/supergenpass.browser.js 3rd/supergenpass.min.js',
+	'mv ./supergenpass-lib/dist/supergenpass-lib.browser.js 3rd/supergenpass-lib.min.js',
 	'rm -rf supergenpass-lib'
 ]));
