585d56a982f1d1adb28257fc49ee6363d9963408
Denis Sokolov <denis@sokolov.cc>
2014-05-10 12:00:36 +0900

    Messages: reminder to use notifications

--- src/messages.js
+++ src/messages.js
@@ -10,3 +10,10 @@ storage.unseen('instructions').then(function(){
 		url: chrome.extension.getURL('src/options/options.html#instructions')
 	});
 });
+
+
+/**
+ * You should use notifications to inform about the updates
+ * without opening a new tab
+ * https://developer.chrome.com/extensions/notifications
+ */
